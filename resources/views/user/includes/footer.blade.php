e4ccb6efefd9e247219c0bbe65d82c4f6d850c9e
desiputripertiwi <desiputripertiwi@gmail.com>
2018-08-04 12:52:01 +0700

    footer

--- resources/views/user/includes/footer.blade.php
+++ resources/views/user/includes/footer.blade.php
@@ -1,22 +1,11 @@
 <footer class="site-footer">
-        <div class="footer-cover-title flex justify-content-center align-items-center">
-            
-        </div><!-- .site-footer -->
-
         <div class="footer-content-wrapper">
             <div class="container">
                 <div class="row">
-                    <div class="col-12">
-                        
-                        <div class="copyright-info">
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> <a href="#">Dinas Sosial Tangerang Selatan </a><i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Spies</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        </div><!-- copyright-info -->
-
-                        <div class="footer-social">
-                            <ul class="flex justify-content-center align-items-center">
-                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <div class="col-12" style="margin-left:100px">
+                        <div class="footer-social" >
+                            <ul class="flex justify-content-right align-items-right">
+                                <li><a href="#"><i class="fab fa-instagram" style="color:red; size:50px"></i></a></li>
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
                                 <li><a href="#"><i class="fab fa-youtube"></i></a></li>
@@ -24,6 +13,14 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> <a hr
                         </div><!-- footer-social -->
                     </div><!-- col -->
                 </div><!-- row -->
+    
+                <!--<div class="row">
+                    <div class="copyright-info">
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. 
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> <a href="#">Dinas Sosial Tangerang Selatan </a><i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Spies</a>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. 
+                    </div><!-- copyright-info 
+                </div>-->
             </div><!-- container -->
         </div><!-- footer-content-wrapper -->
     </footer><!-- site-footer -->
