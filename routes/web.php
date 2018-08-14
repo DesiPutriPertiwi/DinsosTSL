218d990327158dd0e648634aa48d4c39fc9f38c4
desiputripertiwi <desiputripertiwi@gmail.com>
2018-08-15 02:21:15 +0700

    resos

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Beranda
 Route::get('/','user\HomeController@index');
-Route::get('/dayasos&fpm', 'user\DayasosController@index');
-Route::get('/resos', 'user\ResosController@index');
+
+
+// Resos
+Route::get('/konten-resos-kemiskinan', 'user\ResosController@kemiskinan');
+Route::get('/konten-resos-korbanbencana', 'user\ResosController@korban');
+Route::get('/konten-resos-keterlantaran', 'user\ResosController@keterlantaran');
+Route::get('/konten-resos-ketunaansosial', 'user\ResosController@ketunaan');
+Route::get('/konten-resos-kedisabilitasan', 'user\ResosController@kedisabilitasan');
+Route::get('/konten-resos-ktk', 'user\ResosController@ktk');
+Route::get('/data-resos-uap', 'user\ResosController@dataUap');
+Route::get('/data-resos-anak', 'user\ResosController@dataAnak');
+Route::get('/data-resos-lansia', 'user\ResosController@dataLansia');
+Route::get('/data-resos-abc', 'user\ResosController@dataAbc');
+
+
+// Dayasos
+Route::get('/dayasos&fpm-keseluruhan', 'user\DayasosController@index');
+Route::get('/dayasos&fpm-rincian', 'user\DayasosController@rinci');
+
+
+//linjamsos 
 Route::get('/linjamsos', 'user\LinjamsosController@index');
