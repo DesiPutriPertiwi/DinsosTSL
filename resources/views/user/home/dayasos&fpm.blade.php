21f071ac90147e5b2102a97f278ca2e7adb280c4
desiputripertiwi <desiputripertiwi@gmail.com>
2018-08-14 23:09:29 +0700

    update dayasos terbaru

--- resources/views/user/home/dayasos&fpm.blade.php
+++ resources/views/user/home/dayasos&fpm.blade.php
@@ -6,37 +6,7 @@
     <script src='frontEnd/modal/bootstrap.min.js'></script>
 <!--.Modal bootstrap-->
 
-<header class="site-header">
-    <div class="header-bar">
-        <div class="container-fluid">
-            <div class="row align-items-center">
-                <div class="col-md-1" style="margin-top:10px; margin-left:50px">
-                    <img src="{{ asset('image/logo/dinsos.png')}}" width="80px" hight="80px"/>
-                </div>
-                <div class="col-md-4">
-                    <span style="color:#ffffff">
-                        <h1 style="text-shadow: 2px 2px 5px #040505">Dinas Sosial</h1>
-                        <h4 style="text-shadow: 2px 2px 5px #040505">Kota Tangerang Selatan</h4>
-                    </span>
-                </div>
-                <div class="col-md-3">
-                    <img src="{{ asset('image/logo/pkh.png')}}" width="110px" hight="110px" style="margin-left:5px;margin-right:20px"/>
-                    <img src="{{ asset('image/logo/tksk.png')}}" width="75px" hight="75px" style="margin-right:20px"/>
-                    <img src="{{ asset('image/logo/tagana.png')}}" width="45px" hight="45px" />
-                </div>
-                <div class="col-md-3 input-group" style="margin-left:20px">
-                    <input class="form-control" placeholder="Search" type="search" name="search"/>
-                    <button type="button" class="btn btn-primary fas fa-search"> </button>
-                </div>
-            </div><!-- .row -->
-            <div class="row">
-                @include('user.includes.menu')
-            </div> <!-- .row --> 
-
-        </div><!-- container-fluid -->
-    </div><!-- header-bar -->
-</header><!-- .site-header -->
-
+@include('user.includes.header')
 
 <div class="contents-section">
     <div class="container">
@@ -108,13 +78,13 @@
                         <!--Box 1.2-->
                         <div class="col-12 col-md-6">
                             <figure class="featured-image" data-toggle="modal" data-target="#myModal1">
-                                <a href="#"> <img src="data/dayasos/Hasil rekap verval kec.jpg" alt="fesival+celebration"> </a>
+                                <a href="#"> <img src="data/dayasos/dayasos rekap kec.png" alt="fesival+celebration"> </a>
                             </figure><!-- featured-image -->
 
                             <div class="content-wrapper">
                                 <div class="entry-content">
                                     <div class="entry-header" data-toggle="modal" data-target="#myModal1">
-                                        <h2>Rekap Hasil Verifikasi Validasi Kecamatan 2017</h2> <br><br>
+                                        <h2>Rekap Hasil Verifikasi Validasi Kecamatan 2017</h2>
                                     </div><!-- entry-header -->
                                 </div><!-- entry-content -->
                             </div><!-- content-wrapper -->
@@ -137,7 +107,7 @@
                                           Hasil rekapitulasi verifikasi dan validasi data individu, rumah tangga, dan keluarga Kota Tangerang Selatan.
                                     </p>
                                     <figure class="featured-image">
-                                        <img src="data/dayasos/Hasil rekap verval kec.jpg" alt="fesival+celebration">
+                                        <img src="data/dayasos/dayasos rekap kec.png" alt="fesival+celebration">
                                     </figure> <!--featured-image -->
                                     
                                 </div>
@@ -163,7 +133,7 @@
                         <!--Box 2.1-->
                         <div class="col-12 col-md-4">
                             <figure class="featured-image" data-toggle="modal" data-target="#myModal2">
-                                <a href="#"> <img src="data/dayasos/Hasil rekap verval kel.jpg" alt="fesival+celebration"> </a>
+                                <a href="#"> <img src="data/dayasos/rekap rinci kec kel.jpg" alt="fesival+celebration"> </a>
                             </figure><!-- featured-image -->
 
                             <div class="content-wrapper">
@@ -192,7 +162,7 @@
                                           Hasil rekapitulasi verifikasi dan validasi data individu, rumah tangga, dan keluarga Kota Tangerang Selatan.
                                     </p>
                                     <figure class="featured-image">
-                                        <img src="data/dayasos/Hasil rekap verval kec.jpg" alt="fesival+celebration">
+                                        <img src="data/dayasos/rekap rinci kec kel.jpg" alt="fesival+celebration">
                                     </figure> <!--featured-image -->
                                     
                                 </div>
@@ -209,8 +179,8 @@
 
                         <!--Box 2.2-->
                         <div class="col-12 col-md-4">
-                            <figure class="featured-image">
-                                <a href="#" data-toggle="modal" data-target="#myModal3"> <img src="data/dayasos/Hasil rekap verval kec.jpg" alt="fesival+celebration"> </a>
+                            <figure class="featured-image" data-toggle="modal" data-target="#myModal3">
+                                <a href="#" > <img src="data/dayasos/rekap rinci kec.jpg" alt="fesival+celebration"> </a>
                             </figure><!-- featured-image -->
 
                             <div class="content-wrapper">
@@ -239,7 +209,7 @@
                                           Hasil rekapitulasi verifikasi dan validasi data individu, rumah tangga, dan keluarga Kota Tangerang Selatan.
                                     </p>
                                     <figure class="featured-image">
-                                        <img src="data/dayasos/Hasil rekap verval kec.jpg" alt="fesival+celebration">
+                                        <img src="data/dayasos/rekap rinci kec.jpg" alt="fesival+celebration">
                                     </figure> <!--featured-image -->
                                     
                                 </div>
@@ -256,14 +226,14 @@
 
                         <!--Box 2.3-->
                         <div class="col-12 col-md-4">
-                            <figure class="featured-image">
-                                <a href="#" data-toggle="modal" data-target="#myModal4"> <img src="data/dayasos/Hasil rekap verval kec.jpg" alt="fesival+celebration"> </a>
+                            <figure class="featured-image" data-toggle="modal" data-target="#myModal4">
+                                <a href="#" > <img src="data/dayasos/rekap rinci kec kk.jpg" alt="fesival+celebration"> </a>
                             </figure><!-- featured-image -->
 
                             <div class="content-wrapper">
                                 <div class="entry-content">
                                     <div class="entry-header" data-toggle="modal" data-target="#myModal4">
-                                        <h2>Rincian Hasil Verifikasi Validasi KK 2017</h2>
+                                        <h2>Rincian Hasil Verifikasi Validasi Kecamatan KK 2017</h2>
                                     </div><!-- entry-header -->
                                 </div><!-- entry-content -->
                             </div><!-- content-wrapper -->
@@ -276,7 +246,7 @@
                             
                                 <!-- Modal Header -->
                                 <div class="modal-header">
-                                    <h4 class="modal-title">Rincian Hasil Verifikasi Validasi KK 2017</h4>
+                                    <h4 class="modal-title">Rincian Hasil Verifikasi Validasi Kecamatan KK 2017</h4>
                                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                                 </div>
                                 
@@ -286,7 +256,7 @@
                                           Hasil rekapitulasi verifikasi dan validasi data individu, rumah tangga, dan keluarga Kota Tangerang Selatan.
                                     </p>
                                     <figure class="featured-image">
-                                        <img src="data/dayasos/Hasil rekap verval kec.jpg" alt="fesival+celebration">
+                                        <img src="data/dayasos/rekap rinci kec kk.jpg" alt="fesival+celebration">
                                     </figure> <!--featured-image -->
                                     
                                 </div>
--- resources/views/user/home/linjamsos.blade.php
+++ resources/views/user/home/linjamsos.blade.php
@@ -1,37 +1,6 @@
 @extends('user.layout.master')
 
-<header class="site-header">
-    <div class="header-bar">
-        <div class="container-fluid">
-            <div class="row align-items-center">
-                <div class="col-md-1" style="margin-top:10px; margin-left:50px">
-                    <img src="{{ asset('image/logo/dinsos.png')}}" width="80px" hight="80px"/>
-                </div>
-                <div class="col-md-4">
-                    <span style="color:#ffffff">
-                        <h1 style="text-shadow: 2px 2px 5px #040505">Dinas Sosial</h1>
-                        <h4 style="text-shadow: 2px 2px 5px #040505">Kota Tangerang Selatan</h4>
-                    </span>
-                </div>
-                <div class="col-md-3">
-                    <img src="{{ asset('image/logo/pkh.png')}}" width="110px" hight="110px" style="margin-left:5px;margin-right:20px"/>
-                    <img src="{{ asset('image/logo/tksk.png')}}" width="75px" hight="75px" style="margin-right:20px"/>
-                    <img src="{{ asset('image/logo/tagana.png')}}" width="45px" hight="45px" />
-                </div>
-                <div class="col-md-3 input-group" style="margin-left:20px">
-                    <input class="form-control" placeholder="Search" type="search" name="search"/>
-                    <button type="button" class="btn btn-primary fas fa-search"> </button>
-                </div>
-            </div><!-- .row -->
-            <div class="row">
-                @include('user.includes.menu')
-            </div> <!-- .row --> 
-
-        </div><!-- container-fluid -->
-    </div><!-- header-bar -->
-</header><!-- .site-header -->
-
-
+@include('user.includes.header')
 
 <div class="contents-section">
         <!--content coming soon-->
